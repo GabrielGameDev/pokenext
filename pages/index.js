a75ffbde5ecd8a29f5e725aedb735a27ea79a6df
GabrielGameDev <gabrielgamedev@gmail.com>
2022-10-20 17:16:48 -0300

    Acesso a API do Pokemon
    Lista dos pokemons na home

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,39 @@
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export async function getStaticProps(){
+  const maxPokemons = 151
+  const api = 'https://pokeapi.co/api/v2/pokemon/'
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`)
+  const data = await res.json()
+
+  console.log(data)
+
+  data.results.forEach((item, index) => {
+
+    item.id = index + 1
+
+  })
+
+  return {
+    props:{
+      pokemons: data.results
+    }
+  }
+
+}
+
+export default function Home({pokemons}) {
+  console.log(pokemons)
   return (
-   <h1>PokeNext</h1>
+   <div>
+    <h1>PokeNext</h1>
+    <ul>
+      {pokemons.map((pokemon) => (
+        <li key={pokemon.id}>{pokemon.name}</li>
+      ))}
+    </ul>
+   </div>
+   
   )
 }
